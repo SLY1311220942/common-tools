be7c81678133aed2fb7e9593c50fadf117e22c08
盛领宜 <sheng.ly_wh@www.sand.com>
2019-05-15 11:09:41 +0800

    提交

--- src/main/java/com/sly/common/tools/gui/CommonToolLayout.java
+++ src/main/java/com/sly/common/tools/gui/CommonToolLayout.java
@@ -1,13 +1,18 @@
 package com.sly.common.tools.gui;
 
-import java.awt.Color;
-import java.awt.TextField;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-
-import com.sly.common.tools.listener.ModelGeneratorListener;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 
 /**
  * 布局类
@@ -15,50 +20,60 @@ import com.sly.common.tools.listener.ModelGeneratorListener;
  * @author sly
  * @time 2019年5月14日
  */
-public class CommonToolLayout{
+public class CommonToolLayout {
 
 	public void layout() {
-
-		
-
-		
-
-		
-		
-
-		
 		// 布局框
 		JFrame jFrame = new JFrame("Common-Tools");
-		
+
 		jFrame.setLocation(200, 200);
 		jFrame.setSize(1000, 600);
-		// 菜单栏
-		JMenuBar jMenuBar = new JMenuBar();
-		JMenu menu1 = new JMenu("模型生成");
-		JMenu menu2 = new JMenu("Mybatis");
-		JMenu menu3 = new JMenu("UUID生成");
-		JMenu menu4 = new JMenu("时间生成");
-		JMenu menu5 = new JMenu("正则验证");
+
+		// 选项卡
+		JTabbedPane jTabbedPane = new JTabbedPane();
+		JPanel jPanel1 = new JPanel();
+		JPanel jPanel1_1 = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
+		JPanel jPanel1_2 = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
+		JPanel jPanel2 = new JPanel();
+		JPanel jPanel3 = new JPanel();
+		JPanel jPanel4 = new JPanel();
+		JPanel jPanel5 = new JPanel();
+		JTextArea jTextArea = new JTextArea();
 		
+		JButton jButton1 = new JButton("生成模型类");
+		JButton jButton2 = new JButton("复制模型类");
+		jTextArea.setBounds(0, 0, 600, 100);
+		jPanel1_1.add(jButton1);
+		jPanel1_1.add(jButton2);
+		jPanel1_2.add(jTextArea);
 		
-		TextField textField = new TextField();
-		textField.setBounds(40, 20, jFrame.getWidth()-100, 50);
-		textField.setBackground(Color.white);
-		ModelGeneratorListener modelGeneratorListener = new ModelGeneratorListener(textField);
-		menu1.addActionListener(modelGeneratorListener);
+		jPanel1.add(jPanel1_1);
+		jPanel1.add(jPanel1_2);
+
+		jTabbedPane.add("模型生成", jPanel1);
+		jTabbedPane.add("Mybatis", jPanel2);
+		jTabbedPane.add("UUID生成", jPanel3);
+		jTabbedPane.add("时间生成", jPanel4);
+		jTabbedPane.add("正则验证", jPanel5);
+
 		
+		//TextField textField = new TextField(); 
+		//textField.setBounds(40, 20,jFrame.getWidth()-100, 50); 
+		//textField.setBackground(Color.white);
+		//ModelGeneratorListener modelGeneratorListener1 = new ModelGeneratorListener(textField);
+		
+
 		// 将菜单加入菜单栏
-		jMenuBar = addMenuToMenuBar(jMenuBar, menu1, menu2, menu3, menu4, menu5);
+
 		// 不设置位置自动放最上面
-		jFrame.setJMenuBar(jMenuBar);
+
 		
+		//ModelGeneratorListener modelGeneratorListener2 = new ModelGeneratorListener(textField); 
+		//JButton button1 = new JButton("按钮一");
+		//jFrame.add(button1); 
+		//button1.addActionListener(modelGeneratorListener2);
 		
-		
-		
-		
-		
-		
-		
+		jFrame.add(jTabbedPane);
 		
 		jFrame.setVisible(true);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -79,4 +94,5 @@ public class CommonToolLayout{
 		}
 		return jMenuBar;
 	}
+
 }
